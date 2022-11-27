9382f1f1864fcf3e0b176e33c3f56417051c6eea
Patrick McDermott <pattty847@gmail.com>
2022-11-27 09:02:41 -0500

    Minor push

--- Chapter 2/Strings and Text.py
+++ Chapter 2/Strings and Text.py
@@ -6,6 +6,12 @@ line = "asdf fjdks: afed, fdke, asfg,       foo"
 
 # Match text at start or end of string
 filename = "spam.txt"
-print(filename.endswith("txt"))
+# print(filename.endswith("txt"))
 
 
+# Align strings of text
+text = "hello world"
+# print(text.ljust(20))
+# print(text.rjust(20))
+# print(text.center(20))
+# print(text.center(20, "*"))
